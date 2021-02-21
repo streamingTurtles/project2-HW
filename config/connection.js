a381403de1314f63112540d95093aa75fdb280b8
Margaret Elson <margaret.a.elson@gmail.com>
2021-02-21 11:28:02 -0500

    Need to rearrange the page but I'm liking it

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.SECPASSWORD,
+    password: "Dre525252",
     database: "burgers_db",
   });
 }
