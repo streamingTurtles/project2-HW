16423d256926a73bd60401082d3641392766da8d
Margaret Elson <margaret.a.elson@gmail.com>
2021-02-23 19:04:22 -0500

    Add buttons are working and getting the order to appear in the order container. Need to connect sequelize to really take the next big step in getting this to work

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,77 +3,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.info("DOM loaded");
   }
 
-  // UPDATE
-  const changeEatBtns = document.querySelectorAll(".change-Devoured");
+const addButtons = document.querySelectorAll(".add-buttons");
 
-  // Set up the event listener for the create button
-  if (changeEatBtns) {
-    changeEatBtns.forEach((button) => {
-      button.addEventListener("click", (e) => {
-        // console.log("test");
-        // Grabs the id of the element that goes by the name, "id"
-        const id = e.target.getAttribute("data-id");
-        const newEat = true;
-        const newEatState = {
-          devoured: newEat,
-        };
+addButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    //put the server code here
+    var newItem = document.createElement("li")
+    newItem.classList.add("list-group-item")
+    newItem.innerText = e.target.dataset.name
+    document.querySelector(".ordercontainer").append(newItem)
+  })
+})
 
-        fetch(`/api/burger/${id}`, {
-          method: "PUT",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
 
-          // make sure to serialize the JSON body
-          body: JSON.stringify(newEatState),
-        }).then((response) => {
-          // Check that the response is all good
-          // Reload the page so the user can see the new quote
-          if (response.ok) {
-            // console.log(`changed eat to: ${newEat}`);
-            location.reload("/");
-          } else {
-            alert("something went wrong!");
-          }
-        });
-      });
-    });
-  }
 
-  // CREATE
-  const createBurgerBtn = document.getElementById("create-form");
 
-  if (createBurgerBtn) {
-    createBurgerBtn.addEventListener("submit", (e) => {
-      e.preventDefault();
 
-      // Grabs the value of the textarea that goes by the name, "quote"
-      const newBurger = {
-        name: document.getElementById("br").value.trim(),
-        // devoured: document.getElementById("devoured"),
-      };
 
-      // Send POST request to create a new quote
-      fetch("/api/burger", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
 
-        // make sure to serialize the JSON body
-        body: JSON.stringify(newBurger),
-      }).then(() => {
-        // Empty the form
-        document.getElementById("br").value = "";
-        // console.log(fetch);
-        // Reload the page so the user can see the new quote
-        console.log("Created a new burger!");
-        location.reload();
-      });
-    });
-  }
+
+
+
+
 
   const deleteBurgerBtns = document.querySelectorAll(".delete-burger");
 
