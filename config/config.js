8263f863309c7c21fc8d10ab9551a21fc0d8f947
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-03-02 12:35:44 -0500

    updated with adding back in .env & added updated seed.sql data

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+module.exports = {
+
+    "development": {
+        "username": "root",
+        "password": process.env.DB_PASSWORD,
+        "database": "youreats_db",
+        "host": "localhost",
+        "dialect": "mysql",
+        "port": 3306
+      },
+      "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+      },
+      "production": {
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+      }
+
+
+
+
+}
